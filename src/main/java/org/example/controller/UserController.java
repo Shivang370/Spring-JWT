9ae5fcd4a9cfc155dc32284e9e211ec0132a29b9
Shivang <shivang.tripathi@credable.in>
2022-03-16 14:15:32 +0530

    Implemented JWT Authorization for Hello-World

---
+++ src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping({"/hello"})
+    public String PrintHello()
+    {
+        return "Hello World !!";
+    }
+}
---
+++ src/main/java/org/example/service/JwtUserDetailsService.java
@@ -0,0 +1,21 @@
+package org.example.service;
+import java.util.ArrayList;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("javainuse".equals(username)) {
+            return new User("javainuse", "$2a$10$e59rGaFvpijWXLh03j0aZOzBYQNrIRIjlB8sGwLvBL35fecblsW1m",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+}
